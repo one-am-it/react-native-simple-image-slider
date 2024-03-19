111efcf4bb6f9878baaae7c1f1864ba2c1962248
david98 <davidevolta98@gmail.com>
2024-03-19 17:54:33 +0100

    refactor: add typescript as devDependency, remove @fastify/deepmerge

--- src/SimpleImageSliderThemeProvider.tsx
+++ src/SimpleImageSliderThemeProvider.tsx
@@ -2,7 +2,6 @@ import React, { type PropsWithChildren, useMemo } from 'react';
 import { type DefaultTheme, ThemeProvider } from 'styled-components/native';
 import type { SimpleImageSliderTheme } from './@types/styled';
 import type { Subset } from './@types/common';
-import { merge } from './utils/merge';
 
 export type SimpleImageSliderThemeProviderProps = PropsWithChildren<{
     overrides?: Subset<SimpleImageSliderTheme>;
@@ -13,42 +12,42 @@ export default function SimpleImageSliderThemeProvider({
     overrides,
 }: SimpleImageSliderThemeProviderProps) {
     const theme: DefaultTheme = useMemo(
-        () =>
-            merge(
-                {
-                    colors: {
-                        pageCounterBackground: '#D3D3D3',
-                        pageCounterBorder: '#000000',
-                        fullScreenCloseButton: '#FFFFFF',
-                        descriptionContainerBorder: '#FFFFFF',
-                    },
-                    styles: {
-                        spacing: {
-                            xxs: 2,
-                            xs: 4,
-                            s: 8,
-                            m: 16,
-                            l: 20,
-                            xl: 40,
-                        },
-                        borderRadius: {
-                            xs: 2,
-                            s: 4,
-                            m: 8,
-                            l: 16,
-                            xl: 24,
-                        },
-                        borderWidth: {
-                            xs: 1,
-                            s: 2,
-                            m: 4,
-                            l: 8,
-                            xl: 16,
-                        },
-                    },
+        () => ({
+            colors: {
+                pageCounterBackground: '#D3D3D3',
+                pageCounterBorder: '#000000',
+                fullScreenCloseButton: '#FFFFFF',
+                descriptionContainerBorder: '#FFFFFF',
+                ...overrides?.colors,
+            },
+            styles: {
+                spacing: {
+                    xxs: 2,
+                    xs: 4,
+                    s: 8,
+                    m: 16,
+                    l: 20,
+                    xl: 40,
+                    ...overrides?.styles?.spacing,
                 },
-                overrides ?? {}
-            ) as DefaultTheme,
+                borderRadius: {
+                    xs: 2,
+                    s: 4,
+                    m: 8,
+                    l: 16,
+                    xl: 24,
+                    ...overrides?.styles?.borderRadius,
+                },
+                borderWidth: {
+                    xs: 1,
+                    s: 2,
+                    m: 4,
+                    l: 8,
+                    xl: 16,
+                    ...overrides?.styles?.borderWidth,
+                },
+            },
+        }),
         [overrides]
     );
 
