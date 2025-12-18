e58c7984839bc7ad360397c02b007352197d1518
david98 <davidevolta98@gmail.com>
2025-12-18 15:07:26 +0100

    fix: fix example project metro configuration

--- example/metro.config.js
+++ example/metro.config.js
@@ -1,13 +1,14 @@
 const path = require('path');
-const escape = require('escape-string-regexp');
-const { getDefaultConfig } = require('@expo/metro-config');
-const exclusionList = require('metro-config/src/defaults/exclusionList');
+const { getDefaultConfig } = require('expo/metro-config');
 const pak = require('../package.json');
 
 const root = path.resolve(__dirname, '..');
 const modules = Object.keys({ ...pak.peerDependencies });
 
 const defaultConfig = getDefaultConfig(__dirname);
+
+// Simple regex escape function to replace escape-string-regexp
+const escapeRegExp = (string) => string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
 
 /**
  * Metro configuration
@@ -26,9 +27,9 @@ const config = {
     resolver: {
         ...defaultConfig.resolver,
 
-        blacklistRE: exclusionList(
-            modules.map((m) => new RegExp(`^${escape(path.join(root, 'node_modules', m))}\\/.*$`))
-        ),
+        blockList: modules
+            .map((m) => new RegExp(`^${escapeRegExp(path.join(root, 'node_modules', m))}\\/.*$`))
+            .concat(defaultConfig.resolver.blockList),
 
         extraNodeModules: modules.reduce((acc, name) => {
             acc[name] = path.join(__dirname, 'node_modules', name);
