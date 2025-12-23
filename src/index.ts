6c91089dce9d8b4a7c09b1994c21066a2fa47928
david98 <davidevolta98@gmail.com>
2025-12-23 11:37:39 +0100

    fix: fix library exports

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,5 @@
-export {
-    Slider,
-    SliderContent,
-    SliderPageCounter,
-    SliderCorner,
-    SliderFullScreen,
-    SliderCloseButton,
-    SliderDescription,
-    SliderEmpty,
-} from './primitives';
-
-export type {
-    SliderProps,
-    SliderContentProps,
-    SliderPageCounterProps,
-    SliderCornerProps,
-    SliderFullScreenProps,
-    SliderCloseButtonProps,
-    SliderDescriptionProps,
-    SliderEmptyProps,
-} from './primitives';
+export * from './primitives';
 
 export { useSlider } from './context/slider-context';
-export type { SliderItem, SliderContextValue } from './types/context';
-export type { PinchToZoomStatus } from './types/pinch-to-zoom';
+
+export type * from './types';
---
+++ src/types/index.ts
@@ -0,0 +1,5 @@
+export type * from './common';
+export type * from './context';
+export type * from './icons';
+export type * from './pinch-to-zoom';
+export type * from './slider-state';
